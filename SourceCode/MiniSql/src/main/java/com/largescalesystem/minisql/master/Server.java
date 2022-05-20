e4edaf66644a6fb683a598133e5cc73166502856
HoranceRoinson <3386875092@qq.com>
2022-05-20 19:06:22 +0800

    完善Master功能

--- SourceCode/MiniSql/src/main/java/com/largescalesystem/minisql/master/Server.java
+++ SourceCode/MiniSql/src/main/java/com/largescalesystem/minisql/master/Server.java
@@ -1,19 +1,23 @@
 package com.largescalesystem.minisql.master;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.Objects;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(2000);
         System.out.println("Master is waiting for connection");
         for(;;){
             Socket client = server.accept();
             ClientHandler clientHandler = new ClientHandler(client);
             clientHandler.start();
+            RegionManager.listenNode();
         }
     }
 
@@ -29,8 +33,17 @@ public class Server {
             super.run();
             System.out.println("New client connected, address: " + socket.getInetAddress() + ", port: " + socket.getPort());
             try {
-                String sql = String.valueOf(socket.getOutputStream());
-                List<String> tableInfo = RegionManager.getNodeList();
+                // 监听节点
+//                RegionManager.listenNode();
+                BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String temp = input.readLine();
+                String sql = input.readLine();
+                System.out.println("CLIENT REQUEST: " + sql);
+                ArrayList<String> tableInfo = RegionManager.getNodeList();
+//                ArrayList<String> tableInfo = new ArrayList<>();
+//                tableInfo.add("127.0.0.1,1001,3306,123,root,3,school,student,teacher_slave");
+//                tableInfo.add("127.0.0.1,1001,3306,123,root,3,school,student,teacher_slave");
+//                System.out.println(tableInfo);
                 PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
                 String tableName = null;
                 if (Objects.equals(sql.split(" ")[0], "insert") || Objects.equals(sql.split(" ")[0], "delete")) {
@@ -38,19 +51,31 @@ public class Server {
                 }
                 else if(Objects.equals(sql.split(" ")[0], "select")){
                     tableName = sql.split(" ")[3];
-
                 }
-                for(String nodeInfo : tableInfo){
-                    String [] list = nodeInfo.split(",");
-                    boolean flag = false;
-                    for(String s: list){
-                        if(Objects.equals(s, tableName) || Objects.equals(s.split("_")[0], tableName)){
-                            flag = true;
-                            break;
+                else if(Objects.equals(sql.split(" ")[0], "create")){
+//                    tableName = sql.split(" ")[2];
+                    tableName = sql.split("\\(")[0].split(" ")[2];
+                }
+
+//                System.out.println(tableName);
+                if(Objects.equals(sql.split(" ")[0], "create")){
+                    System.out.println("TARGET REGION SERVER: " + RegionManager.loadBalance(tableInfo));
+                    printWriter.println(RegionManager.loadBalance(tableInfo));
+                }
+                else {
+                    for (String nodeInfo : tableInfo) {
+                        String[] list = nodeInfo.split(",");
+                        boolean flag = false;
+                        for (String s : list) {
+                            if (Objects.equals(s, tableName) || Objects.equals(s.split("_")[0], tableName)) {
+                                flag = true;
+                                break;
+                            }
                         }
-                    }
-                    if(flag){
-                        printWriter.println(nodeInfo);
+                        if (flag) {
+                            System.out.println("TARGET REGION SERVER: " + nodeInfo);
+                            printWriter.println(nodeInfo);
+                        }
                     }
                 }
                 printWriter.close();
